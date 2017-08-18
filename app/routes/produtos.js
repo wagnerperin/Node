040cf4d7cd7844a66d0fde4c27a815d0608914ef
Wagner de Andrade Perin <wagnerperin@WagnerMac.local>
2017-08-18 16:23:06 -0300

    Incluindo biblioteca mysql

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,17 @@
 module.exports = function(app) {
     app.get('/produtos', (req, res) => {
-        res.render("produtos/lista");
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : '',
+            database : 'casadocodigo'
+        });
+
+        connection.query('select * from livros', (err, result) => {
+            res.send(result);
+        });
+
+        connection.end();
     });
 }
