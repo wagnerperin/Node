8e006105c584b89cb6b38303b9a476fe2124d2eb
Wagner de Andrade Perin <wagnerperin@WagnerMac.local>
2017-08-19 10:56:51 -0300

    Content Negociation (Enviando json)

---
+++ cadastra-livros-terminal.js
@@ -0,0 +1,27 @@
+var http = require('http');
+
+var config = {
+    hostname: 'localhost',
+    port: 3000,
+    path: '/produtos',
+    method: 'post',
+    headers: {
+        'Accept':'application/json',
+        'Content-type':'application/json'
+    }
+};
+
+var client = http.request(config, function(res){
+    console.log(res.statusCode);
+    res.on('data', function(body){
+        console.log('Corpo:' + body);
+    });
+});
+
+var produto = {
+    titulo: 'Mais sobre nome',
+    descricao: 'node, javascript e http',
+    preco: 100
+};
+
+client.end(JSON.stringify(produto));
