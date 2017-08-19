d89e31a588f8f7adaba5b675f9205e67823b32a6
Wagner de Andrade Perin <wagnerperin@WagnerMac.local>
2017-08-19 15:34:47 -0300

    imoplementando websockets.io

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var app = require('./config/express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.listen(3000, () => {
+app.set('io', io);
+
+http.listen(3000, () => {
     console.log('Servidor iniciado.');
 });
