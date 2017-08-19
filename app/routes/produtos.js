805bbc2eb8fbac4b792c88b06a9f33c478f7a82a
Wagner de Andrade Perin <wagnerperin@WagnerMac.local>
2017-08-19 11:34:15 -0300

    Tratando erros de validacao

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -19,11 +19,22 @@ module.exports = function(app) {
     });
 
     app.get('/produtos/form', function(req, res){
-        res.render('produtos/form');
+        res.render('produtos/form', {errosValidacao: {}});
     });
 
     app.post('/produtos', function(req, res){
         var produto = req.body;
+
+        req.assert('titulo', 'Titulo e obrigatorio').notEmpty();
+        req.assert('preco', 'Formato invalido').isFloat();
+        
+
+        var erros = req.validationErrors();
+
+        if(erros) {
+            res.render('produtos/form', {errosValidacao : erros});
+            return;  
+        }
 
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
