37859a4622dccb00967dbdb32857420a4a9c8599
Wagner de Andrade Perin <wagnerperin@WagnerMac.local>
2017-08-19 10:30:22 -0300

    Salvando livros

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,13 +1,28 @@
 module.exports = function(app) {
     app.get('/produtos', (req, res) => {
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
 
 
-        produtosBanco.lista((err, result) => {
+        produtosDAO.lista((err, result) => {
             res.render('produtos/lista', {lista : result});
         });
 
         connection.end();
+    });
+
+    app.get('/produtos/form', function(req, res){
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos', function(req, res){
+        var produto = req.body;
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        produtosDAO.salva(produto, function(err, result){
+           res.redirect('/produtos');
+        });
     });
 }
