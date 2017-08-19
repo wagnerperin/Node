ce780d72a7e67468f06b3b9186432da1e396b9e1
Wagner de Andrade Perin <wagnerperin@WagnerMac.local>
2017-08-19 12:42:15 -0300

    testes usando o supertest

--- test/produtos.js
+++ test/produtos.js
@@ -1,21 +1,11 @@
-var http = require('http');
-var assert = require('assert');
+var express = require('../config/express')();
+var request = require('supertest')(express);
 
 describe('#ProdutosController', function(){
-    it('#listagem json', function(done){
-        var config = {
-            hostname : 'localhost',
-            port : 3000,
-            path : '/produtos',
-            headers : {
-                'Accept' : 'application/json'
-            }
-        };
-
-        http.get(config, function(res){
-            assert.equal(res.statusCode, 200);
-            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
-            done();
-        });
+    it('#listagem json', function(done){ 
+        request.get('/produtos')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     });
 });
