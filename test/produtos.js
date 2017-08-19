844d2409a93d6789202e913816b0527f3471fac9
Wagner de Andrade Perin <wagnerperin@WagnerMac.local>
2017-08-19 14:10:26 -0300

    otimizando os testes e limpando tabelas.

--- test/produtos.js
+++ test/produtos.js
@@ -2,6 +2,15 @@ var express = require('../config/express')();
 var request = require('supertest')(express);
 
 describe('#ProdutosController', function(){
+    beforeEach(function(done){
+        var conn = express.infra.connectionFactory();
+        conn.query("delete from livros", function(ex, res){
+            if(!ex){
+                done();
+            }
+        });
+    });
+    
     it('#listagem json', function(done){ 
         request.get('/produtos')
             .set('Accept', 'application/json')
