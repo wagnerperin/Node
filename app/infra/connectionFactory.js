f430aeff9012405ea507e8295004cd0c0ab82ecf
Wagner de Andrade Perin <wagnerperin@WagnerMac.local>
2017-08-19 15:52:19 -0300

    renderizando erros com middleware

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -6,7 +6,7 @@ function createDBConnection() {
                 host : 'localhost',
                 user : 'root',
                 password : '',
-                database : 'casadocodigo'
+                database : 'casadocodigo22'
             });
     }
 
